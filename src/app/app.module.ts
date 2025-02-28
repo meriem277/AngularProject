96020741b7e8b64b17e872f3960ba21a44959c3a
meriem277 <meriem.rehouma@esprit.tn>
2025-02-28 18:15:14 +0100

    generation les compoenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ResidencesComponent } from './residences/residences.component';
+import { ResidencesComponent } from './residences/residence/residences.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ResidenceDetailsComponent } from './residences/residence-details/residence-details.component';
+import { AddResidenceComponent } from './residences/add-residence/add-residence.component';
+import { ApartmentsComponent } from './apartments/apartments/apartments.component';
+import { ApartmentsByResidenceComponent } from './apartments/apartments-by-residence/apartments-by-residence.component';
+import { AddApartmentComponent } from './apartments/add-apartment/add-apartment.component'; // ✅ Ajout du module FormsModule
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResidencesComponent
+    ResidencesComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent,
+    ResidenceDetailsComponent,
+    AddResidenceComponent,
+    ApartmentsComponent,
+    ApartmentsByResidenceComponent,
+    AddApartmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
